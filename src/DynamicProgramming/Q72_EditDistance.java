a73bb84fddef23cb87a55719bf15a13894ec5ecb
564258080@qq.com <564258080@qq.com>
2019-09-07 11:19:15 -0400

    solutions

---
+++ src/DynamicProgramming/Q72_EditDistance.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class Q72_EditDistance {
+
+    public static void main(String[] args){
+        Q72_EditDistance a=new Q72_EditDistance();
+        System.out.println(a.minDistance("horse","ros"));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int m=word1.length()+1,n=word2.length()+1;
+        int [][] dp=new int[m][n];
+        for (int i=0;i<m;i++)
+            Arrays.fill(dp[i],0);
+        for (int i=0;i<m;i++) dp[i][0]=i;
+        for (int j=0;j<n;j++) dp[0][j]=j;
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=Math.min(Math.min(dp[i-1][j]+1,dp[i][j-1]+1),dp[i-1][j-1]+1);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
